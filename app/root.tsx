c0a8dd641de00de695de8a27a309e89f5bfeb797
ckolhackerthon2233 <felixcole83@gmail.com>
2025-09-14 01:28:59 +0300

    Authentication

--- app/root.tsx
+++ app/root.tsx
@@ -19,7 +19,7 @@ export const links: Route.LinksFunction = () => [
   },
   {
     rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,ops,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
 ];
 
@@ -33,7 +33,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+      <script src="https://js.puter.com/v2/"></script>
+
+      {children}
         <ScrollRestoration />
         <Scripts />
       </body>
